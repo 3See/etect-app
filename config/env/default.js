35b494609afaf02ad55f24c917ab54e2058f0e73
Alex Clark <aclark2089@gmail.com>
2015-10-07 01:42:11 -0400

    Fixed Tab and Project description to fit eTect theme

--- config/env/default.js
+++ config/env/default.js
@@ -3,7 +3,7 @@
 module.exports = {
   app: {
     title: 'eTect Clinical Study',
-    description: 'Full-Stack JavaScript with MongoDB, Express, AngularJS, and Node.js',
+    description: 'Clinical Study Application for use by eTect',
     keywords: 'mongodb, express, angularjs, node.js, mongoose, passport',
     googleAnalyticsTrackingID: process.env.GOOGLE_ANALYTICS_TRACKING_ID || 'GOOGLE_ANALYTICS_TRACKING_ID'
   },
