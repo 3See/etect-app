99e4ee6d1d8584662f66dbcb03ac204e9ff9225b
KevinKrames <kevinkrames@gmail.com>
2015-10-07 10:05:00 -0400

    Added Angular JS + HTML file

---
+++ modules/core/client/controllers/script-subject.js
@@ -0,0 +1,35 @@
+// Code goes here
+
+var myApp = angular.module('app', ['ngTable']);
+
+myApp.controller('MainCtrl', function ($scope, ngTableParams){
+  $scope.info = [{fullName: "John Doe", patientID:"123456789", readerID:"123456789", startDate:"01/02/03", endDate:"02/03/04", address:"123 S Main St, Gainesville, Fl, 21601", address2:"5050 W University Ave.PO Box 90909, Gainesville Fl, 32607", phone:"813-555-1234", phone2:"813-555-4321", emergencyContacts:[{name:"Jane Doe", relationship:"Wife", phone:"352-555-5678"}, {name:"Jack Doe", relationship:"Brother", phone:"813-555-3636"}]}];
+  $scope.newItem = "";
+  
+  $scope.addItem = function(){
+    console.log("in add");
+    if ($scope.newItem !== ""){
+      var addItem = {name:$scope.newItem, done:false};
+      $scope.todos.push(addItem);
+      $scope.newItem = "";
+
+    }
+  }
+    
+  $scope.deleteItem = function(index){
+    console.log("in delete");
+    $scope.todos.splice(index, 1);
+  }
+
+  $scope.completeItem = function(index){
+    console.log("completed item");
+    $scope.todos[index].done = true;
+  }
+
+  $scope.uncompleteItem = function(index) {
+    console.log("uncompleted item");
+    $scope.todos[index].done = false;
+  }
+    
+  
+});
