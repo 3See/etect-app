1aebd6609313daa78dd349ef258a3beadab8921b
Alex Clark <aclark2089@gmail.com>
2015-10-07 10:21:08 -0400

    Fixed new app declaration and made controller a controller of core modules instead, fixed names and included services for controller, added to subjectinfo.client

--- modules/core/client/controllers/subjectinfo.client.controller.js
+++ modules/core/client/controllers/subjectinfo.client.controller.js
@@ -1,8 +1,7 @@
-// Code goes here
+'use strict';
 
-var myApp = angular.module('app', ['ngTable']);
 
-myApp.controller('MainCtrl', function ($scope, ngTableParams){
+angular.module('core').controller('SubjectInfoController', ['$scope', 'Authentication', '$location',
   $scope.info = [{fullName: "John Doe", patientID:"123456789", readerID:"123456789", startDate:"01/02/03", endDate:"02/03/04", address:"123 S Main St, Gainesville, Fl, 21601", address2:"5050 W University Ave.PO Box 90909, Gainesville Fl, 32607", phone:"813-555-1234", phone2:"813-555-4321", emergencyContacts:[{name:"Jane Doe", relationship:"Wife", phone:"352-555-5678"}, {name:"Jack Doe", relationship:"Brother", phone:"813-555-3636"}]}];
   $scope.newItem = "";
   
