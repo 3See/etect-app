caa9bf5b45290ca00003e2b7d637d5058a80f417
Alex Clark <aclark2089@gmail.com>
2015-10-07 21:41:25 -0400

    Fixed some controllers not being named correctly or including their service injections as function parameters

--- modules/core/client/controllers/subjectinfo.client.controller.js
+++ modules/core/client/controllers/subjectinfo.client.controller.js
@@ -1,7 +1,7 @@
 'use strict';
 
-
-angular.module('core').controller('SubjectInfoController', ['$scope', 'Authentication', '$state', function ($scope) {
+angular.module('core').controller('SubjectInfoController', ['$scope', 'Authentication', '$state',
+    function ($scope, Authentication, $state) {
 
     // This provides Authentication context.
     $scope.authentication = Authentication;
