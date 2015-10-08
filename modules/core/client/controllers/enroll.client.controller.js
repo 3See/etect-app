f9ad4718f74ba54ba1c71ad1c9b23b390a5905d0
Alex Clark <aclark2089@gmail.com>
2015-10-07 20:31:32 -0400

    Fixed enroll controller to use state and fixed angular functionality of controller

--- modules/core/client/controllers/enroll.client.controller.js
+++ modules/core/client/controllers/enroll.client.controller.js
@@ -6,7 +6,24 @@ angular.module('core').controller('EnrollController', ['$scope', 'Authentication
     $scope.authentication = Authentication;
 
     if(!Authentication.user) {
-    	$location.path('/authentication/signin');
+    	$state.go('sign-in');
+    }
+
+    $scope.submit = function () {
+      $http.post('/api/client/enrollment', $scope.credentials).success(function (response) {
+        // If successful we assign the response to the global user model
+        $scope.authentication.user = response;
+
+        // And redirect to the previous or home page
+        $state.go($state.previous.state.name || 'home', $state.previous.params);
+      }).error(function (response) {
+        $scope.error = response.message;
+      });
+    }
+
+
+    $scope.cancel = function () {
+      $state.go($state.previous.state.name || 'home', $state.previous.params);
     }
   }
 ]);
