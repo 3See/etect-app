26f0c3ed680762eb1b05ce036e7fd190b6e73f59
nicolas <naugle92@ufl.edu>
2015-10-09 04:31:00 -0400

    Study Page Accordion now running on U.I. Bootstrap. View and Controller updated.

--- modules/core/client/controllers/studyinfo.client.controller.js
+++ modules/core/client/controllers/studyinfo.client.controller.js
@@ -12,3 +12,71 @@ angular.module('core').controller('StudyInfoController', ['$scope', 'Authenticat
 
   }
 ]);
+
+
+angular.module('core').controller('AccordionCtrl', function ($scope) {
+  $scope.oneAtATime = true;
+
+  $scope.groups = [
+    {
+      title: 'Group 1',
+      content: 'John Doe'
+    },
+    {
+      title: 'Group 2',
+      content: 'John Smith'
+    },
+    {
+    	title: 'Control',
+    	content: 'Jon Jones'
+    }
+  ];
+
+  $scope.items = ['Item 1', 'Item 2', 'Item 3'];
+
+  $scope.addItem = function() {
+    var newItemNo = $scope.items.length + 1;
+    $scope.items.push('Item ' + newItemNo);
+  };
+
+  $scope.status = {
+    isFirstOpen: true,
+    isFirstDisabled: false
+  };
+});
+
+
+angular.directive('accordionGroup', function() {
+  return {
+    require:'^accordion',         // We need this directive to be inside an accordion
+    restrict:'EA',
+    transclude:true,              // It transcludes the contents of the directive into the template
+    replace: true,                // The element containing the directive will be replaced with the template
+    templateUrl:'template/accordion/accordion-group.html',
+    scope: {
+      heading: '@',               // Interpolate the heading attribute onto this scope
+      isOpen: '=?',
+      isDisabled: '=?'
+    },
+    controller: function() {
+      this.setHeading = function(element) {
+        this.heading = element;
+      };
+    },
+    link: function(scope, element, attrs, accordionCtrl) {
+      accordionCtrl.addGroup(scope);
+
+      scope.$watch('isOpen', function(value) {
+        if ( value ) {
+          accordionCtrl.closeOthers(scope);
+        }
+      });
+
+      scope.toggleOpen = function() {
+        if ( !scope.isDisabled ) {
+          scope.isOpen = !scope.isOpen;
+        }
+      };
+    }
+  };
+})
