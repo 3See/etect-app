39d5100da6f9e1a8b6ae4b389e667ea40d1b22d3
nicolas <naugle92@ufl.edu>
2015-10-13 04:03:32 -0400

    disabled the directive temporarily and added a table to the accordion sections

--- modules/core/client/controllers/studyinfo.client.controller.js
+++ modules/core/client/controllers/studyinfo.client.controller.js
@@ -12,3 +12,74 @@ angular.module('core').controller('StudyInfoController', ['$scope', 'Authenticat
 
   }
 ]);
+
+
+angular.module('core').controller('AccordionCtrl', function ($scope) {
+  $scope.oneAtATime = true;
+
+  $scope.groups = [
+    {
+      title: 'Group 1',
+      content: 'John Doe',
+      reader: 'R001',
+      complience: '97%'
+    },
+    {
+      title: 'Group 2',
+      content: 'John Smith',
+      reader: 'R015',
+      complience: '93%'
+    },
+    {
+    	title: 'Control',
+    	content: 'Jon Jones',
+    	reader: 'R054',
+      	complience: '85%'
+    }
+  ];
+
+  $scope.items = ['Item 1', 'Item 2', 'Item 3'];
+
+  $scope.addItem = function() {
+    var newItemNo = $scope.items.length + 1;
+    $scope.items.push('Item ' + newItemNo);
+  };
+
+  $scope.status = {
+    isFirstOpen: true,
+    isFirstDisabled: false
+  };
+})
+.directive('groupTable', function() {
+  return { 
+    restrict:'E',
+    transclude:true,              // It transcludes the contents of the directive into the template
+    replace: true,                // The element containing the directive will be replaced with the template
+    templateUrl:'group-table.client.view.html',
+    scope: {
+      heading: '@',               // Interpolate the heading attribute onto this scope
+      isOpen: '=?',
+      isDisabled: '=?'
+    },
+    controller: function() {
+      this.setHeading = function(element) {
+        this.heading = element;
+      };
+    },
+    link: function(scope, element, attrs, accordionCtrl) {
+      accordionCtrl.addGroup(scope);
+
+      scope.$watch('isOpen', function(value) {
+        if ( value ) {
+          accordionCtrl.closeOthers(scope);
+        }
+      });
+
+      scope.toggleOpen = function() {
+        if ( !scope.isDisabled ) {
+          scope.isOpen = !scope.isOpen;
+        }
+      };
+    }
+  };
+})
--- modules/core/client/directives/core.client.directives.js
+++ modules/core/client/directives/core.client.directives.js
@@ -1,66 +1,35 @@
-'use strict';
+/*'use strict';
 
 angular.module('core')
-    .directive('groupTable', function () {
-        return { //return this as an object
-            restrict: 'E',
-            templateUrl: '../views/group-table.client.view.html'//, //where the html is we are going to use in this tag
-            /*controller: function () {
-                this.setHeading = function (element) {
-                    this.heading = element;
-                };
-            },
-            link: function (scope, element, attrs, accordionCtrl) {
-                accordionCtrl.addGroup(scope);
+.controller('AccordionController', function ($scope) {
+      $scope.oneAtATime = true;
 
-                scope.$watch('isOpen', function (value) {
-                    if (value) {
-                        accordionCtrl.closeOthers(scope);
-                    }
-                });
+      $scope.groups = [
+        {
+          title: 'Dynamic Group Header - 1',
+          content: 'Dynamic Group Body - 1'
+        },
+        {
+          title: 'Dynamic Group Header - 2',
+          content: 'Dynamic Group Body - 2'
+        }
+      ];
 
-                scope.toggleOpen = function () {
-                    if (!scope.isDisabled) {
-                        scope.isOpen = !scope.isOpen;
-                    }
-                };
-            },
-            controllerAs: groupCtl //what controller will be called by*/
+      $scope.items = ['Item 1', 'Item 2', 'Item 3'];
 
+      $scope.addItem = function() {
+        var newItemNo = $scope.items.length + 1;
+        $scope.items.push('Item ' + newItemNo);
+      };
 
-
-        };
-    });
-
-//OLD CONTROLLER
-// Try to fit this functionality into the controller function above.
-
-/*
-$scope.oneAtATime = true;
-
-$scope.groups = [
-    {
-        title: 'Group 1',
-        content: 'John Doe'
-    },
-    {
-        title: 'Group 2',
-        content: 'John Smith'
-    },
-    {
-        title: 'Control',
-        content: 'Jon Jones'
-    }
-];
-
-$scope.items = ['Item 1', 'Item 2', 'Item 3'];
-
-$scope.addItem = function() {
-    var newItemNo = $scope.items.length + 1;
-    $scope.items.push('Item ' + newItemNo);
-};
-
-$scope.status = {
-    isFirstOpen: true,
-    isFirstDisabled: false
-};*/
+      $scope.status = {
+        isFirstOpen: true,
+        isFirstDisabled: false
+      };
+    })
+.directive('groupTable', function () {
+    return { //return this as an object
+        templateUrl: '../views/group-table.client.view.html'//, //where the html is we are going to use in this tag
+    };
+});
+*/
