51898a0ed2d3dc41da74de5171d53158bfb31619
nicolas <naugle92@ufl.edu>
2015-10-13 22:27:56 -0400

    cleaned up code in studyview.client.view.html. Cannot get templateUrl working in the directive

--- modules/core/client/controllers/studyinfo.client.controller.js
+++ modules/core/client/controllers/studyinfo.client.controller.js
@@ -49,37 +49,63 @@ angular.module('core').controller('AccordionCtrl', function ($scope) {
     isFirstOpen: true,
     isFirstDisabled: false
   };
-})
-.directive('groupTable', function() {
+});
+
+
+
+angular.module('core').directive('groupTable', function() {
   return { 
     restrict:'E',
-    transclude:true,              // It transcludes the contents of the directive into the template
-    replace: true,                // The element containing the directive will be replaced with the template
-    templateUrl:'group-table.client.view.html',
-    scope: {
-      heading: '@',               // Interpolate the heading attribute onto this scope
-      isOpen: '=?',
-      isDisabled: '=?'
-    },
-    controller: function() {
-      this.setHeading = function(element) {
-        this.heading = element;
-      };
-    },
-    link: function(scope, element, attrs, accordionCtrl) {
-      accordionCtrl.addGroup(scope);
-
-      scope.$watch('isOpen', function(value) {
-        if ( value ) {
-          accordionCtrl.closeOthers(scope);
-        }
-      });
-
-      scope.toggleOpen = function() {
-        if ( !scope.isDisabled ) {
-          scope.isOpen = !scope.isOpen;
-        }
-      };
+    //transclude:true,              // It transcludes the contents of the directive into the template
+    //replace: true,                // The element containing the directive will be replaced with the template
+    template:'<div>\
+  <div ng-controller="AccordionCtrl">\
+      <script type="text/ng-template" id="group-template.html">\
+        <div class="panel {{panelClass || \'panel-default\'}}">\
+          <div class="panel-heading">\
+            <h4 class="panel-title" style="color:#fa39c3">\
+              <a href tabindex="0" class="accordion-toggle" ng-click="toggleOpen()" uib-accordion-transclude="heading"><span\
+                ng-class="{\'text-muted\': isDisabled}">{{heading}}</span></a>\
+            </h4>\
+          </div>\
+          <div class="panel-collapse collapse" uib-collapse="!isOpen">\
+            <div class="panel-body" style="text-align: right" ng-transclude></div>\
+          </div>\
+        </div>\
+      </script>\
+      <accordion close-others="oneAtATime">\
+        <accordion-group ng-repeat="group in groups">\
+            <accordion-heading>\
+                {{group.title}}\
+\
+                <button type="button" class="btn btn-default pull-right studypage" data-toggle="collapse" data-target="#demo1" ui-sref="group-info">\
+                    Group Data\
+                </button>\
+           \
+            </accordion-heading>\
+              <table class="studyTable">\
+                  <tr>\
+                      <th>Name</th>\
+                      <th>Reader Number</th>\
+                      <th>Complience</th>\
+                  </tr>\
+                  <tr>\
+                      <td>{{group.content}}</td>\
+                      <td>{{group.reader}}</td>\
+                      <td>{{group.complience}}</td>\
+                  </tr>\
+              </table>\
+        </accordion-group>\
+      </accordion>\
+  </div>\
+</div>',
+   //scope: {
+   //   heading: '@',               // Interpolate the heading attribute onto this scope
+   //   isOpen: '=?',
+   //   isDisabled: '=?'
+   // },
+    controller: function($scope) {
+      //alert('controller');
     }
   };
-})
+});
